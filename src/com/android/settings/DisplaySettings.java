05181bf356a99c1cf27827935bf07fcf2c2e4ff2
Schubi <schubi@erlangen.ccc.de>
2013-03-02 19:10:59 +0100

    Port Dualpanel for Settings from Beergang
    Change-Id: I3c07fd9081c549ed3b01f11f0fefa8c0fad7b43c
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/display_settings.xml
    	src/com/android/settings/DisplaySettings.java
    
    Conflicts:
    	res/values/strings.xml

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -70,6 +70,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+    private static final String KEY_DUAL_PANEL = "force_dualpanel";
 
     // Strings used for building the summary
     private static final String ROTATION_ANGLE_0 = "0";
@@ -83,6 +84,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private CheckBoxPreference mVolumeWake;
     private Preference mCustomLabel;
+    private CheckBoxPreference mDualPanel;
     private PreferenceScreen mDisplayRotationPreference;
     private WarnedListPreference mFontSizePref;
 
@@ -163,6 +165,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                         Settings.System.VOLUME_WAKE_SCREEN, 0) == 1);
             }
         }
+        mDualPanel = (CheckBoxPreference) findPreference(KEY_DUAL_PANEL);
+        mDualPanel.setChecked(Settings.System.getBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL, false));
 
         mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
         updateCustomLabelTextSummary();
@@ -413,7 +417,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     mVolumeWake.isChecked() ? 1 : 0);
             return true;
 
-
+        } else if (preference == mDualPanel) {
+            Settings.System.putBoolean(getContentResolver(), Settings.System.FORCE_DUAL_PANEL, ((CheckBoxPreference) preference).isChecked());
+            return true;
 
         } else if (preference == mCustomLabel) {
             AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
