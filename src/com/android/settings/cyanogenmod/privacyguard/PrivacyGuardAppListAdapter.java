288b36d51e1bc4b17974b42b5f8b8c605f455daa
Lars Greiss <kufikugel@googlemail.com>
2013-07-02 01:46:10 +0200

    Settings: Introduce Privacy Guard Manager
    Due that on the one hand a lot user are not familiar
    with permissions and how to read them and on the
    other user want to have a faster easy access to
    setup privacy guard for several apps this small
    manager assist the user to setup.
    
    Privacy Guard Manager shows a list off all installed apps
    Per default it shows only user apps which have critical
    permissions privacy guard can protect.
    The user can optional show in the list as well all system apps
    or turn off the filter to show all apps undependent of the
    permission the app has. To activate privacy guard for an
    app a simple click on the entry activates it so that the user
    can go fast trough the list. Longpress opens app detail page.
    Initial help dialog tells the user how to use it.
    
    Due the filter user who are unfimiliar with permissions can
    fast see which apps are effected without deeper knowledge
    
    cheers
    
    SlimRoms
    
    PatchSet3:
    - go inline with AOSP style
    - move PrivacyGuard by default into manager
    - change behaviour off active icons
    
    PatchSet4:
    - change app icon size to 36dip
    
    PatchSet5:
    - string change
    
    PatchSet6:
    - add reset option to disable privacy guard for
      all shown apps in the list
    
    PatchSet7:
    - Cleaned up the intro dialog a bit.
    
    PatchSet8:
    - cleanup
    - fix alignment of top checkbox
    
    Change-Id: I6bc886393bf90f8daed92b66d5fee1ce9e1accd0

---
+++ src/com/android/settings/cyanogenmod/privacyguard/PrivacyGuardAppListAdapter.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2013 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.cyanogenmod.privacyguard;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.settings.R;
+
+public class PrivacyGuardAppListAdapter extends BaseAdapter {
+
+    private LayoutInflater mInflater;
+
+    private List<PrivacyGuardManager.AppInfo> mApps;
+    private Map<String, Drawable> mIcons;
+    private Drawable mDefaultImg;
+
+    private Context mContext;
+
+    //constructor
+    public PrivacyGuardAppListAdapter(Context context) {
+        mContext = context;
+        mInflater = LayoutInflater.from(mContext);
+        // set the default icon till the actual app icon is loaded in async task
+        mDefaultImg = mContext.getResources().getDrawable(R.drawable.ic_launcher);
+    }
+
+    @Override
+    public int getCount() {
+        return mApps.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mApps.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        PrivacyGuardAppViewHolder appHolder;
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.privacy_guard_manager_list_row, null);
+
+            // creates a ViewHolder and children references
+            appHolder = new PrivacyGuardAppViewHolder();
+            appHolder.title = (TextView) convertView.findViewById(R.id.app_title);
+            appHolder.icon = (ImageView) convertView.findViewById(R.id.app_icon);
+            appHolder.privacyGuardIcon = (ImageView) convertView.findViewById(R.id.app_privacy_guard_icon);
+            convertView.setTag(appHolder);
+        } else {
+            appHolder = (PrivacyGuardAppViewHolder) convertView.getTag();
+        }
+
+        PrivacyGuardManager.AppInfo app = mApps.get(position);
+
+        appHolder.title.setText(app.title);
+        if (mIcons == null || mIcons.get(app.packageName) == null) {
+            appHolder.icon.setImageDrawable(mDefaultImg);
+        } else {
+            appHolder.icon.setImageDrawable(mIcons.get(app.packageName));
+        }
+
+        int privacyGuardDrawableResId = app.privacyGuardEnabled
+                ? R.drawable.ic_privacy_guard : R.drawable.ic_privacy_guard_off;
+        appHolder.privacyGuardIcon.setImageResource(privacyGuardDrawableResId);
+
+        return convertView;
+    }
+
+    public void setListItems(List<PrivacyGuardManager.AppInfo> list) {
+        mApps = list;
+    }
+
+    public void setIcons(Map<String, Drawable> icons) {
+        mIcons = icons;
+    }
+
+    /**
+     * App view holder used to reuse the views inside the list.
+     */
+    public static class PrivacyGuardAppViewHolder {
+        TextView title;
+        ImageView icon;
+        ImageView privacyGuardIcon;
+    }
+}
